f7309601d5aa2409903f5cfbe64891e4e7ff3a72
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-20 02:15:53 +0700

    resetup api route

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TravelCompanyController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\TravelTripController;
+use App\Http\Controllers\TravelCompanyController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,4 +35,11 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     // Route for show list of travel company data
     Route::get('/travels-company', [TravelCompanyController::class, 'index']);
+
+    // Route for show list of booking data
+    Route::get('/my-booking', [BookingController::class, 'index']);
+    Route::post('/booking', [BookingController::class, 'store']);
+    Route::get('/booking/{id}', [BookingController::class, 'show']);
+    Route::post('/booking/{id}/payment-proof', [BookingController::class, 'uploadPaymentProof']);
+    Route::post('/booking/{id}/cancel', [BookingController::class, 'cancel']);
 });
