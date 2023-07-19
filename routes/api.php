56e495f5f7a3b87f49dc15002ba2c09e2f8478de
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-19 23:06:19 +0700

    setup all route API

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TravelCompanyController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TravelTripController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +18,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+
+    // Route for update profile, get user data, and logout
+    Route::post('/update-profile', [UserController::class, 'updateProfile']);
+    Route::get('/me', [UserController::class, 'me']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+
+    // Route for show list of trip with travel company data
+    Route::get('/travels', [TravelTripController::class, 'index']);
+    Route::post('/travels/filter/', [TravelTripController::class, 'filter']);
+
+    // Route for show list of travel company data
+    Route::get('/travels-company', [TravelCompanyController::class, 'index']);
 });
