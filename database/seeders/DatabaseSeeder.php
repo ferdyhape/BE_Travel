0ef90882fe106061cffadce074855823add78137
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-19 23:02:02 +0700

    call all seeder on databaseseeder for seeding data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Travel;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            UserSeeder::class,
+            TravelSeeder::class,
+            TravelTripSeeder::class,
+        ]);
     }
 }
