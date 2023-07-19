3f3a123e7e94666888c2ee6e79d53129c98712df
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-20 02:13:24 +0700

    add model and migration of passenger

---
+++ app/Models/Passengers.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Passengers extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'booking_id',
+        'name',
+        'phone_number',
+    ];
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class);
+    }
+}
