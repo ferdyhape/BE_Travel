ecfaa34f9b8a3dc6a78317a240bea229ca569e5f
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-19 22:59:47 +0700

    add migration and model for travel(company) table

---
+++ app/Models/Travel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Travel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'travels';
+    protected $fillable = [
+        'name',
+        'description',
+        'profile_photo_path',
+    ];
+
+    public function trips()
+    {
+        return $this->hasMany(TravelTrip::class);
+    }
+}
